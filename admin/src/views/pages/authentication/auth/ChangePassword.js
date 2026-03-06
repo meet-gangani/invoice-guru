7d756e5101640c707854cfca5366673f7cd36245
Vivek Vaghani <vivekvaghani307@gmail.com>
2026-03-06 21:22:26 +0530

    Refactor ChangePassword to use Auth wrappers

--- admin/src/views/pages/authentication/auth/ChangePassword.js
+++ admin/src/views/pages/authentication/auth/ChangePassword.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
-import { Box, Button, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, Typography } from '@mui/material'
+import { useSelector } from 'react-redux'
+import { useTheme } from '@mui/material/styles'
+import { Box, Button, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, Stack, Typography, useMediaQuery } from '@mui/material'
 import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
 import Logo from '../../../../ui-component/Logo'
 import UserService from '../../../../services/user.service'
 import { useNavigate } from 'react-router-dom'
+import AuthWrapper1 from '../AuthWrapper'
+import AuthCardWrapper from '../AuthCardWrapper'
+import AnimateButton from 'ui-component/extended/AnimateButton'
 
 const ChangePassword = () => {
   const navigate = useNavigate()
+  const theme = useTheme()
+  const matchDownSM = useMediaQuery(theme.breakpoints.down('md'))
+  const customization = useSelector((state) => state.customization)
 
   const [ showPassword, setShowPassword ] = useState({
     currentPassword: false,
@@ -49,137 +57,148 @@ const ChangePassword = () => {
   }
 
   return (
-      <>
-        <Box
-            sx={{
-              backgroundColor: '#eef2f6',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100vh',
-              width: '100vw',
-              padding: '20px',
-              boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)'
-            }}>
-          <Grid container direction="column" justifyContent="center" spacing={2}>
-            <Grid item xs={12}>
-              <Box
-                  sx={{
-                    borderRadius: '20px',
-                    padding: 4,
-                    marginX: 'auto',
-                    backgroundColor: '#fff',
-                    alignItems: 'center',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    width: '30%'
-                  }}
-              >
-                <Logo/>
-                <Typography sx={{ color: '#F7BE15', fontSize: 24, fontWeight: 800, marginY: '24px' }}>
-                  Change Your Password
-                </Typography>
-                <Box sx={{ display: 'flex', width: '100%', flexDirection: 'column', gap: 2, marginBottom: 2 }}>
-                  <FormControl>
-                    <InputLabel htmlFor="outlined-adornment-email-login">Current Password</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-email-login"
-                        type={showPassword.currentPassword ? 'text' : 'password'}
-                        value={passwords.currentPassword}
-                        name="email"
-                        onChange={(e) => setPasswords({ ...passwords, currentPassword: e.target.value })}
-                        endAdornment={
-                          <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={() => setShowPassword((prevState) => ({
-                                  ...prevState,
-                                  currentPassword: !prevState.currentPassword
-                                }))}
-                                edge="end"
+      <AuthWrapper1>
+        <Grid container direction="column" justifyContent="flex-end" sx={{ minHeight: '100vh' }}>
+          <Grid item xs={12}>
+            <Grid container justifyContent="center" alignItems="center" sx={{ minHeight: 'calc(100vh - 68px)' }}>
+              <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+                <AuthCardWrapper>
+                  <Grid container spacing={2} alignItems="center" justifyContent="center">
+                    <Grid item sx={{ mb: 3 }}>
+                      <Logo/>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Grid container direction={matchDownSM ? 'column-reverse' : 'row'} alignItems="center" justifyContent="center">
+                        <Grid item>
+                          <Stack alignItems="center" justifyContent="center" spacing={1}>
+                            <Typography color={theme.palette.secondary.main} gutterBottom variant={matchDownSM ? 'h3' : 'h2'}>
+                              Change Your Password
+                            </Typography>
+                            <Typography variant="caption" fontSize="16px" textAlign={matchDownSM ? 'center' : 'inherit'}>
+                              Enter your current and new password
+                            </Typography>
+                          </Stack>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                        <FormControl fullWidth sx={{ ...theme.typography.customInput }}>
+                          <InputLabel htmlFor="outlined-adornment-current-password">Current Password</InputLabel>
+                          <OutlinedInput
+                              id="outlined-adornment-current-password"
+                              type={showPassword.currentPassword ? 'text' : 'password'}
+                              value={passwords.currentPassword}
+                              name="currentPassword"
+                              onChange={(e) => setPasswords({ ...passwords, currentPassword: e.target.value })}
+                              endAdornment={
+                                <InputAdornment position="end">
+                                  <IconButton
+                                      aria-label="toggle current password visibility"
+                                      onClick={() => setShowPassword((prevState) => ({
+                                        ...prevState,
+                                        currentPassword: !prevState.currentPassword
+                                      }))}
+                                      edge="end"
+                                      size="large"
+                                  >
+                                    {showPassword.currentPassword ? <Visibility/> : <VisibilityOff/>}
+                                  </IconButton>
+                                </InputAdornment>
+                              }
+                              label="Current Password"
+                              inputProps={{}}
+                          />
+                        </FormControl>
+                        <FormControl fullWidth sx={{ ...theme.typography.customInput }}>
+                          <InputLabel htmlFor="outlined-adornment-new-password">New Password</InputLabel>
+                          <OutlinedInput
+                              id="outlined-adornment-new-password"
+                              type={showPassword.newPassword ? 'text' : 'password'}
+                              value={passwords.newPassword}
+                              name="newPassword"
+                              onChange={(e) => setPasswords({ ...passwords, newPassword: e.target.value })}
+                              endAdornment={
+                                <InputAdornment position="end">
+                                  <IconButton
+                                      aria-label="toggle new password visibility"
+                                      onClick={() => setShowPassword((prevState) => ({
+                                        ...prevState,
+                                        newPassword: !prevState.newPassword
+                                      }))}
+                                      edge="end"
+                                      size="large"
+                                  >
+                                    {showPassword.newPassword ? <Visibility/> : <VisibilityOff/>}
+                                  </IconButton>
+                                </InputAdornment>
+                              }
+                              label="New Password"
+                              inputProps={{}}
+                          />
+                        </FormControl>
+                        <FormControl fullWidth sx={{ ...theme.typography.customInput }}>
+                          <InputLabel htmlFor="outlined-adornment-confirm-password">Confirm Password</InputLabel>
+                          <OutlinedInput
+                              id="outlined-adornment-confirm-password"
+                              type={showPassword.confirmPassword ? 'text' : 'password'}
+                              value={passwords.confirmPassword}
+                              name="confirmPassword"
+                              onChange={(e) => setPasswords({ ...passwords, confirmPassword: e.target.value })}
+                              endAdornment={
+                                <InputAdornment position="end">
+                                  <IconButton
+                                      aria-label="toggle confirm password visibility"
+                                      onClick={() => setShowPassword((prevState) => ({
+                                        ...prevState,
+                                        confirmPassword: !prevState.confirmPassword
+                                      }))}
+                                      edge="end"
+                                      size="large"
+                                  >
+                                    {showPassword.confirmPassword ? <Visibility/> : <VisibilityOff/>}
+                                  </IconButton>
+                                </InputAdornment>
+                              }
+                              label="Confirm Password"
+                              inputProps={{}}
+                          />
+                        </FormControl>
+                        <Box sx={{ mt: 1 }}>
+                          <AnimateButton>
+                            <Button
+                                disableElevation
+                                fullWidth
                                 size="large"
+                                variant="contained"
+                                color="secondary"
+                                sx={{ borderRadius: `${customization.borderRadius}px` }}
+                                onClick={changeUserPassword}
                             >
-                              {showPassword ? <Visibility/> : <VisibilityOff/>}
-                            </IconButton>
-                          </InputAdornment>
-                        }
-                        label="Password"
-                        inputProps={{}}
-                    />
-                  </FormControl>
-                  <FormControl>
-                    <InputLabel htmlFor="outlined-adornment-email-login">New Password</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-email-login"
-                        type={showPassword.newPassword ? 'text' : 'password'}
-                        value={passwords.newPassword}
-                        name="email"
-                        onChange={(e) => setPasswords({ ...passwords, newPassword: e.target.value })}
-                        endAdornment={
-                          <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={() => setShowPassword((prevState) => ({
-                                  ...showPassword,
-                                  newPassword: !prevState.newPassword
-                                }))}
-                                edge="end"
-                                size="large"
-                            >
-                              {showPassword ? <Visibility/> : <VisibilityOff/>}
-                            </IconButton>
-                          </InputAdornment>
-                        }
-                        label="Password"
-                        inputProps={{}}
-                    />
-                  </FormControl>
-                  <FormControl>
-                    <InputLabel htmlFor="outlined-adornment-email-login">Confirm Password</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-email-login"
-                        type={showPassword.confirmPassword ? 'text' : 'password'}
-                        value={passwords.confirmPassword}
-                        name="password"
-                        onChange={(e) => setPasswords({ ...passwords, confirmPassword: e.target.value })}
-                        endAdornment={
-                          <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={() => setShowPassword((prevState) => ({
-                                  ...showPassword,
-                                  confirmPassword: !prevState.confirmPassword
-                                }))}
-                                edge="end"
-                                size="large"
-                            >
-                              {showPassword ? <Visibility/> : <VisibilityOff/>}
-                            </IconButton>
-                          </InputAdornment>
-                        }
-                        label="Password"
-                        inputProps={{}}
-                    />
-                  </FormControl>
-                  <Button
-                      sx={{
-                        backgroundColor: '#F7BE15',
-                        borderRadius: 3,
-                        paddingY: 1,
-                        color: 'white',
-                        '&:hover': {
-                          backgroundColor: '#eab50d',
-                          color: 'gray.300'
-                        }
-                      }}
-                      onClick={changeUserPassword}
-                  >Change Password</Button>
-                </Box>
-              </Box>
+                              Change Password
+                            </Button>
+                          </AnimateButton>
+                        </Box>
+                        <Box sx={{ mt: 1, display: 'flex', justifyContent: 'center' }}>
+                          <Button
+                              variant="text"
+                              color="secondary"
+                              size="small"
+                              onClick={() => navigate('/dashboard')}
+                              sx={{ textTransform: 'none' }}
+                          >
+                            Back to Dashboard
+                          </Button>
+                        </Box>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </AuthCardWrapper>
+              </Grid>
             </Grid>
           </Grid>
-        </Box>
-      </>
+        </Grid>
+      </AuthWrapper1>
   )
 }
 
